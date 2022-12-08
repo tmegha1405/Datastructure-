e43b51a84013234ee3f1ecab381220002c198f78
tmegha1405 <117816288+tmegha1405@users.noreply.github.com>
2022-12-08 11:56:34 +0530

    Add files via upload

--- linkedlist.c
+++ linkedlist.c
@@ -8,46 +8,149 @@ int data;
 struct node *next;
 };
 
-struct node *head=0,newnode;
+struct node *head=NULL;
 void insertfirst()
 {
-newnode=(struct node *)malloc(sizeof(struct node));
-if(newnode==0)
-{
+struct node *newnode;
+newnode=(struct node*)malloc(sizeof(struct node*));
+if(newnode==NULL)
 printf("\n no space");
-}
 else
 {
-printf("enter a integer")
+printf("enter the element to be inserted:");
 scanf("%d",&newnode->data);
-newnode->next=0;
-}
-if(head==0)
-{
+newnode->next=NULL;
+if(head==NULL)
 head=newnode;
-}
 else
 {
 newnode->next=head;
 head=newnode;
 }
-printf("%d inserted into list:");
+printf("\n%d inserted into list:",newnode->data);
 }
+}
+void insertlast()
+{
+struct node *temp,*newnode;
+newnode=(struct node*)malloc(sizeof(struct node*));
+newnode->next=NULL;
+temp=head;
+if(newnode==NULL)
+printf("\n no space available");
+else
+{
+while(temp->next!=NULL)
+temp=temp->next;
+printf("Enter the element to be inserted:");
+scanf("%d",&newnode->data);
+temp->next=newnode;
+printf("\n%d inserted",newnode->data);
+}
+}
+void insertlocation()
+{
+int key;
+struct node *temp,*newnode;
+newnode=(struct node*)malloc(sizeof(struct node*));
+newnode->next=NULL;
+printf("\n Enter value of the node after which the new node to be inserted:");
+scanf("%d",&key);
+temp=head;
+while(temp->data!=key)
+{
+temp=temp->next;
+if(temp==NULL)
+break;
+}
+if(temp->data==key){
+if(newnode==NULL)
+printf("\n no space available");
+else
+{
+printf("Enter the element to be inserted:");
+scanf("%d",&newnode->data);
+temp->next=newnode;
+printf("\n%d inserted after %d",newnode->data,key);
+}
+}
+}
+void deletefirst()
+{
+struct node *temp;
+temp=head;
+head=temp->next;
+printf("%d deleted",temp->data);
+free(temp);
+}
+void deletelast()
+{
+struct node *temp,*p;
+temp=head;
+while(temp->next!=NULL)
+{
+p=temp;
+temp=temp->next;
+}
+printf("%d deleted",temp->data);
+free(temp);
+p->next=NULL;
+}
+
+void deletelocation()
+{
+int pos,i=1;
+struct node *next,*nextnode,*temp;
+printf("Enter the position");
+scanf("%d",&pos);
+temp=head;
+while(i<pos-1)
+{
+temp=temp->next;
+i++;
+}
+nextnode=temp->next;
+temp->next=nextnode->next;
+free(nextnode);
+}
+
+void search()
+{
+struct node *temp;
+int key,pos=0;
+temp=head;
+printf("\nEnter the element to be searched:");
+scanf("%d",&key);
+while(temp->data!=key)
+{
+temp=temp->next;
+pos++;
+if(temp==NULL)
+break;
+} if(temp==NULL)
+{
+printf("\n The %d value not exist",key);
+return;
+}
+if(temp->data==key)
+{
+printf("\n %d is available in location %d",key,pos+1);
+}
+}
+
+
 void display()
 {
 struct node *p;
-if(head==0)
-{
+if(head==NULL)
 printf("\n list is empty\n");
-}
 else
 {
 printf("\nthe elements in the list:\n");
 p=head;
-}
-while(p !=0)
+while(p!=NULL)
 {
-printf("%d",p->data);
+printf("\t%d",p->data);
 p=p->next;
 }
 }
@@ -56,9 +159,9 @@ p=p->next;
 void main()
 {
 int choice;
-printf("\n singly linked list is:");
+printf("\n singly linked list:");
 do{
-printf("\n 1.insert at beginning\n");
+printf("\n 1.insert at beginning\n 2.insert at last \n 3.insert after a location\n 4.Delete first \n 5.Delete last \n 6.delete after a location \n7.search an element \n8.display linked list\n 9.exit\n");
 printf("enter a choice\n");
 scanf("%d",&choice);
 switch(choice)
@@ -66,13 +169,26 @@ switch(choice)
 case 1:
 insertfirst();
 break;
-}
+case 2:insertlast();
+break;
+case 3:insertlocation();
+break;
+case 4: deletefirst();
+break;
+case 5:deletelast();
+break;
+case 6:deletelocation();
+break;
+case 7:search();
+break;
+case 8:display();
+break;
+case 9:exit(0);
 default:
 printf("\n invalid option");
 }
 }
-}
-while(choice != 1)
+while(choice!=9);
 }
 
 
